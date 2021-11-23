9e24ed1e5e7bdbbe0e465fbab5365087a6b4b275
Anton Kamardin <a.kamardin@newlinetechnologies.com>
2021-11-23 22:27:30 +0200

    bla

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import trackConvertRoutes from './routes/track.convert.routes';
 
 const app = express();
 
-app.use('/api/track-convert', trackConvertRoutes);
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   next();
 });
+app.use('/api/track-convert', trackConvertRoutes);
 
 const startApplication = () => {
   app.listen(process.env.PORT || 5000, () =>
